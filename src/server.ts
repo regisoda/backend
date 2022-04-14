f4d0741338500819e49d39e760bb95ba7dd4c75f
Caio Oda <rm87707@fiap.com.br>
2022-04-13 22:41:25 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from "cors"
 import express, { Request, Response } from "express"
 import { prismaClient } from "./middlewares/connection"
 
-const PORT = process.env.PORT || 3000
+const PORT = "https://keyteambackend.herokuapp.com" || 3000
 
 const app = express()
 app.use(bodyParser.json())
